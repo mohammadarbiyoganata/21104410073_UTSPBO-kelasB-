7590c633812906eb7dedd2e493e1f813c9fba012
veynata <119788046+veynata@users.noreply.github.com>
2022-12-04 23:04:29 -0800

    add getter setter

--- UTS_MUSIC/src/KineticCalc/Kinetic.java
+++ UTS_MUSIC/src/KineticCalc/Kinetic.java
@@ -9,9 +9,28 @@ package KineticCalc;
  * @author ASUS
  */
 public class Kinetic {
+    private double M;
+    public double getM(){
+        return M;
+    }
+    public void setM(double M){
+        this.M = M;
+    }
+    private double V;
+    public double getV(){
+        return V;
+    }
+    public void setV(double V){
+        this.V = V;
+    }
     static double kinetic(double M, double V){
         double kinetic;
         kinetic = 0.5 *M*V*V;
+    
+    
+    
+        
+        
         return kinetic;
     }
 }
